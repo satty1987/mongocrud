0f9390a4fc74de53b7e69408a186c0bdb21da87b
Satnam Singh <satnam.singh156@t-mobile.com>
2019-09-01 22:49:10 +0530

    commit

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-const dbName = process.env.NODE_ENV === 'dev' ? 'database-test' : 'database' 
-const url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${dbName}:27017?authMechanism=SCRAM-SHA-1&authSource=admin`
+const auth = {
+  MONGO_INITDB_ROOT_USERNAME: 'admin',
+  MONGO_INITDB_ROOT_PASSWORD: 'admin123'
+}
+
+const dbName = 'ds215338.mlab.com:15338/crud-app'
+const url = `mongodb://${auth.MONGO_INITDB_ROOT_USERNAME}:${auth.MONGO_INITDB_ROOT_PASSWORD}@${dbName}`
 const options = {
   useNewUrlParser: true, 
   reconnectTries: 60, 
   reconnectInterval: 1000
 }
 const routes = require('./routes/routes.js')
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
 const app = express()
 const http = require('http').Server(app)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(require('./auth'));
 app.use('/api', routes)
 app.use((req, res) => {
-  res.status(404)
+  res.status(404).send({ message: 'Route'+req.url+' Not found.' });
 })
 
 MongoClient.connect(url, options, (err, database) => {
@@ -26,7 +32,7 @@ MongoClient.connect(url, options, (err, database) => {
     console.log(`FATAL MONGODB CONNECTION ERROR: ${err}:${err.stack}`)
     process.exit(1)
   }
-  app.locals.db = database.db('api')
+  app.locals.db = database.db('crud-app')
   http.listen(port, () => {
     console.log("Listening on port " + port)
     app.emit('APP_STARTED')
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,51 @@
 const express = require('express')
 
 const Document = require('../models/Document')
-const router = express.Router()
+const router = express.Router();
+var request = require("request");
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: 'https://dev-337333.oktapreview.com/oauth2/default', // required
+  clientId: '0oafkl686pfl21cP10h7'
+});
+
 
 router.get('/documents', (req, res, next) => {
   req.app.locals.db.collection('documents').find({}).toArray((err, result) => {
     if (err) {
-      res.status(400).send({'error': err})
+      res.status(400).send({ 'error': err })
     }
     if (result === undefined || result.length === 0) {
-      res.status(400).send({'error':'No documents in database'})
+      res.status(400).send({ 'error': 'No documents in database' })
     } else {
       res.status(200).send(result)
     }
   })
+})
+
+router.get('/github/:user', (req, response, next) => {
+  var headers = {
+    'User-Agent': 'Super Agent/0.0.1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  var options = {
+    url: `http://api.github.com/users/${req.params.user}`,
+    method: 'GET',
+    headers: headers,
+  };
+  
+  request(options, function (error, resp, body) {
+
+    if (!error && resp.statusCode == 200) {
+      // Print out the response body
+      response.send(body);
+    } else {
+      response.send(body);
+    }
+
+  });
+
 })
 
 router.get('/documents/:id', (req, res, next) => {
@@ -21,10 +53,10 @@ router.get('/documents/:id', (req, res, next) => {
     '_id': req.params.id
   }, (err, result) => {
     if (err) {
-      res.status(400).send({'error': err})
+      res.status(400).send({ 'error': err })
     }
     if (result === undefined) {
-      res.status(400).send({'error':'No document matching that id was found'})
+      res.status(400).send({ 'error': 'No document matching that id was found' })
     } else {
       res.status(200).send(result)
     }
@@ -32,12 +64,13 @@ router.get('/documents/:id', (req, res, next) => {
 })
 
 router.post('/documents', (req, res, next) => {
+  console.log(req);
   const newDocument = new Document(req.body.title, req.body.username, req.body.body)
   req.app.locals.db.collection('documents').insertOne({
     newDocument
   }, (err, result) => {
     if (err) {
-      res.status(400).send({'error': err})
+      res.status(400).send({ 'error': err })
     }
     res.status(200).send(result)
   })
@@ -48,7 +81,7 @@ router.delete('/documents/:id', (req, res, next) => {
     '_id': req.params.id
   }, (err, result) => {
     if (err) {
-      res.status(400).send({'error': err})
+      res.status(400).send({ 'error': err })
     }
     res.status(200).send(result)
   })
@@ -57,19 +90,20 @@ router.delete('/documents/:id', (req, res, next) => {
 router.put('/documents/:id', (req, res, next) => {
   req.app.locals.db.collection('documents').updateOne({
     '_id': req.params.id
-  }, 
-  {$set:
+  },
     {
-      title: req.body.title,
-      username: req.body.username,
-      body: req.body.body
-    }
-  }, (err, result) => {
-    if (err) {
-      res.status(400).send({'error': err})
-    }
-    res.status(200).send(result)
-  })
+      $set:
+      {
+        title: req.body.title,
+        username: req.body.username,
+        body: req.body.body
+      }
+    }, (err, result) => {
+      if (err) {
+        res.status(400).send({ 'error': err })
+      }
+      res.status(200).send(result)
+    })
 })
 
 module.exports = router
